928f85737f87f7896c9efe482662e3030f3e04a5
Ignacio Corres <corresignacio@gmail.com>
2023-10-31 20:14:35 -0300

    add CDN configuration

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'articles',
+    #thirds party:
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -151,6 +153,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static", # os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # in production, we want cdn
+
+from .cdn.conf import * # noqa
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
